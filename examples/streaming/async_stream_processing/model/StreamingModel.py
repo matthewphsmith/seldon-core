abba3fa2b60a25ac06e285b3b08bde67619a8e1f
Alejandro Saucedo <axsauze@gmail.com>
2020-02-22 18:42:46 +0000

    Removed model and local given graph example references streaming

--- examples/streaming/async_stream_processing/model/StreamingModel.py
+++
@@ -1,7 +0,0 @@
-class StreamingModel:
-    def __init__(self):
-        print("INITIALIZING STREAMINGMODEL")
-
-    def predict(self, data, names=[], meta=[]):
-        print(f"Inside predict: data [{data}] names [{names}] meta [{meta}]")
-        return data
