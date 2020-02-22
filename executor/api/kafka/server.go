063b8bd3754d5fd2db60e8747025fda4592d4c39
Alejandro Saucedo <axsauze@gmail.com>
2020-02-22 11:18:20 +0000

    Added logic to push to final deployment output topic

--- executor/api/kafka/server.go
+++ executor/api/kafka/server.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/url"
+	"regexp"
 	"strconv"
 
 	"github.com/Shopify/sarama"
 	"github.com/go-logr/logr"
-	"github.com/seldonio/seldon-core/executor/api/client"
 	"github.com/seldonio/seldon-core/executor/api/metric"
 	"github.com/seldonio/seldon-core/executor/api/payload"
 	v1 "github.com/seldonio/seldon-core/operator/apis/machinelearning/v1"
@@ -30,7 +30,8 @@ type SeldonKafkaApi struct {
 	prometheusPath string
 }
 
-func NewServerKafkaApi(predictor *v1.PredictorSpec, client client.SeldonApiClient, probesOnly bool, serverUrl *url.URL, namespace string, protocol string, deploymentName string, prometheusPath string) *SeldonKafkaApi {
+// TODO: Change Kafka client to client.SeldonClientAPI
+func NewServerKafkaApi(predictor *v1.PredictorSpec, client KafkaClient, probesOnly bool, serverUrl *url.URL, namespace string, protocol string, deploymentName string, prometheusPath string) *SeldonKafkaApi {
 
 	var serverMetrics *metric.ServerMetrics
 	if !probesOnly {
@@ -75,6 +76,10 @@ func (r *SeldonKafkaApi) RecursiveAppendTopicNamesToConsume(pu *v1.PredictiveUni
 	if *pu.Type != v1.MODEL {
 		log.Fatal("Streaming functionality for seldon only supports MODEL predictors, and does not support other seldon deployment component types for now.")
 	}
+	// TODO: Explore support for multiple children for each graph node
+	if len(pu.Children) > 1 {
+		log.Fatal("Streaming functionality for Seldon only supports single children nodes")
+	}
 	for i := 0; i < len(pu.Children); i++ {
 		r.RecursiveAppendTopicNamesToConsume(&pu.Children[i], topics)
 	}
@@ -93,6 +98,26 @@ func (r *SeldonKafkaApi) GetNextModelFromOutputTopic(outputTopic string) *v1.Pre
 	// TODO: Make this work with other methods other than predict
 	reStr := fmt.Sprintf("%s-%s-(.*)-predict-output", r.DeploymentName, r.predictor.Name)
 	re := regexp.MustCompile(reStr)
+	match := re.FindStringSubmatch(outputTopic)
+	if len(match) != 2 {
+		log.Fatalf("Error extracting model name from topic: %s", outputTopic)
+	}
+	modelName := match[1]
+	// TODO: Add functionality to support other methods other than predict and single-child
+	currModel := r.predictor.Graph
+	for {
+		if len(currModel.Children) == 0 {
+			return nil
+		}
+		// TODO: Explore support for multiple children for each graph node
+		if len(currModel.Children) > 1 {
+			log.Fatal("Streaming functionality for Seldon only supports single children nodes")
+		}
+		if currModel.Name == modelName {
+			return &currModel.Children[0]
+		}
+		currModel = &currModel.Children[0]
+	}
 }
 
 func (r *SeldonKafkaApi) Setup(_ sarama.ConsumerGroupSession) error   { return nil }
@@ -100,30 +125,36 @@ func (r *SeldonKafkaApi) Cleanup(_ sarama.ConsumerGroupSession) error { return n
 func (r *SeldonKafkaApi) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
 		r.Log.Info("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		var model *v1.PredictiveUnit
 		if msg.Topic == r.KafkaClient.getDeploymentInputTopicName() {
+			model = r.predictor.Graph
+		} else {
+			model = r.GetNextModelFromOutputTopic(msg.Topic)
+		}
+
+		if model != nil {
 			// Given it's the first topic we want to get the first element in the graph
-			firstModel := r.predictor.Graph
 			port, err := strconv.Atoi(r.ServerUrl.Port())
 			if err != nil {
 				log.Fatalf("Could not convert port string to int: %v", err)
 			}
 			bytesPayload := payload.BytesPayload{Msg: msg.Value}
 			meta := map[string][]string{payload.SeldonPUIDHeader: []string{string(msg.Key)}}
-			r.KafkaClient.Predict(sess.Context(), firstModel.Name, r.ServerUrl.Hostname(), int32(port), &bytesPayload, meta)
+			r.KafkaClient.Predict(sess.Context(), model.Name, r.ServerUrl.Hostname(), int32(port), &bytesPayload, meta)
 		} else {
-			nextModel := r.GetNextModelFromOutputTopic(msg.Topic)
-			if nextModel != nil {
-				port, err := strconv.Atoi(r.ServerUrl.Port())
-				if err != nil {
-					log.Fatalf("Could not convert port string to int: %v", err)
-				}
-				bytesPayload := payload.BytesPayload{Msg: msg.Value}
-				meta := map[string][]string{payload.SeldonPUIDHeader: []string{string(msg.Key)}}
-				r.KafkaClient.Predict(sess.Context(), nextModel.Name, r.ServerUrl.Hostname(), int32(port), &bytesPayload, meta)
+			// TODO: Expose produceMessageToTopic function from kafkaclient instead of sending directly
+			inputTopic := r.KafkaClient.getDeploymentOutputTopicName()
+			message := &sarama.ProducerMessage{
+				Topic: inputTopic,
+				Key:   sarama.ByteEncoder(msg.Key),
+				Value: sarama.ByteEncoder(msg.Value),
 			}
-			else {
-				// If there is no next model then we send the final output to the deploymen output topic
+			partition, offset, err := r.KafkaClient.kafkaProducer.SendMessage(message)
+			r.Log.Info(fmt.Sprintf("Successful message sent to partition [%d] offset [%d] with topic %s", partition, offset, inputTopic))
+			if err != nil {
+				return err
 			}
+			return nil
 		}
 		// Mark messasge as read
 		sess.MarkMessage(msg, "")
